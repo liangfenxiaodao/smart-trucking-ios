90d680ccb00fe301a19df74719ba450c3db4f1e3
Jacky Li <veryfaint@gmail.com>
2014-05-22 22:35:15 +1000

    project structure with cocoapods

---
+++ SmartTrucking/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SmartTrucking
+//
+//  Created by Jacky Li on 22/5/14.
+//  Copyright (c) 2014 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SmartTrucking/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  SmartTrucking
+//
+//  Created by Jacky Li on 22/5/14.
+//  Copyright (c) 2014 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+
+}
