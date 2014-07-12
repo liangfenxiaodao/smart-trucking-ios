457b6d4f20737c82a4b20256f4482b19dc6b498e
jacky li <veryfaint@gmail.com>
2014-07-12 21:58:06 +1000

    rename referenceRate to reference_rate

--- SmartTrucking/Model/Listing.m
+++ SmartTrucking/Model/Listing.m
@@ -14,7 +14,7 @@
   self.arriveTime = [self parseDate:dictionary[@"arrive_time"]];
   self.bidEndingTime = [self parseDate: dictionary[@"bid_ending_time"]];
   self.palletJackRequired = [dictionary[@"pallet_jack_required"] boolValue];
-  self.referenceRate = [dictionary[@"referenceRate"] intValue];
+  self.referenceRate = [dictionary[@"reference_rate"] intValue];
   self.specialCarryingPermitRequired = [dictionary[@"special_carrying_permit_required"] boolValue];
   self.tailgate = dictionary[@"tail_gate"];
   self.vehicleType = dictionary[@"vehicle_type"];
